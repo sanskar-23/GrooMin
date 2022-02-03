83185748321957166856f673695f5b4514ef961a
Sanskar-23 <skapoor_be20@thapar.edu>
2022-02-03 23:29:42 +0530

    Creater Login and Register Page

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3300
 
 
+// set Template Engine
+
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
 // Assets
 app.use(express.static('public'));
 
@@ -13,12 +19,18 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-// set Template Engine
 
-app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+})
 
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
