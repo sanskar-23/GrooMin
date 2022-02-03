90cbf4c9c6f0cd3c391c51a20ef0c4ff8e1a8aec
Sanskar-23 <skapoor_be20@thapar.edu>
2022-02-03 05:32:05 +0530

    initial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3300
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+// set Template Engine
+
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`)
+})
